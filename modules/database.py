62cb17a1d83adf5c015ff6acbdb1979721be701e
Michel LOCHE <m.loche@aliceadsl.fr>
2020-12-28 14:14:54 +0100

    Added database existance control

--- modules/database.py
+++ modules/database.py
@@ -2,6 +2,7 @@
 # -*-  coding: UTF-8
 
 import pymysql as mariadb
+import sys
 
 #########################
 # DATABASE USAGE MODULE #
@@ -14,7 +15,7 @@ def _connect(db_serv,db_admin,db_pass,db_name,db_port=3306):
 		try:
 			conn = mariadb.connect(host=db_serv,user=db_admin,password=db_pass,database=db_name,port=db_port)
 		except mariadb.Error as error:
-			print("error connecting to mariadb platform: {}".format(error))
+			print("error connecting to mariadb platform: {} with _connect".format(error))
 	else:
 		sys.exit("Error invalid values given, for CONNECTING TO A DATABASE. Quit")
 	return(conn)
@@ -45,9 +46,27 @@ def _query_list(db_info,query):
 #control				print(query_result)
 		except:
 			print("could not query {} to database {}".format(query,db_info))
-		print("Releasing the database")
+#control		print("Releasing the database")
 		db_conn.close()
 
 	else:
 		print("Invalid values given for QUERY LIST, DB_INFOS must be a list and QUERY a string or list of strings")
+
+
+def _check_db(db_info):
+	if isinstance(db_info,dict):
+		try:
+			db_info["db_name"]="datatest"
+			db_conn=_connect(db_info["ip_address"],db_info["db_admin"],db_info["db_password"],db_info["db_name"],db_info['db_port'])
+			test_query=_query(db_conn,"show tables from {} ;".format(db_info["db_name"]))
+#control			print("test_query",test_query)
+			if test_query == None :
+#control				print("database is empty")
+				return(False)
+
+			else:
+				sys.exit("Database is not empty")
+		except:
+			sys.exit("Database is not empty")
+
 
